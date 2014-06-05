80b69d38e25b7a0df9c9855e10dc7bd430aae794
Evan Dorsky <enigmoid@gmail.com>
2014-06-04 23:20:15 -0400

    Initial commit

---
+++ public/js/physics.js
@@ -0,0 +1,52 @@
+function() {
+
+	window.vec2 = function(x, y) {
+		var v = this
+
+		v.x = x
+		v.y = y
+
+		// properties
+		v.phase = function() {
+			return Math.atan2(v.y, v.x)
+		}
+
+		v.mag = function() {
+			return Math.sqrt(v.x * v.x, v.y * v.y)
+		}
+
+		v.hat = function() {
+			var mag = v.mag()
+			return new vec2(v.x / mag, v.y / mag)
+		}
+
+		// vector operations
+		v.add = function(w) {
+			return new vec2(v.x + w.x, v.y + w.y)
+		}
+
+		v.sub = function(w) {
+			return new vec2(v.x - w.x, v.y - w.y)
+		}
+
+		v.dot = function(w) {
+			return new vec2(v.x * w.x, v.y * w.y)
+		}
+
+		v.cross = function(w) {
+			return (v.x * w.y) - (v.y * w.x)
+		}
+
+		// scalar operations
+		v.mul = function(a) {
+			return new vec2(v.x * a, v.y * a)
+		}
+
+		v.div = function(a) {
+			return new vec2(v.x / a, v.y / a)
+		}
+
+		return v
+	}
+
+}()
