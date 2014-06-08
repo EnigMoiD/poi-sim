2ca0e6fe8c3f0239da1ce882b688ef0f0e51c5d2
Evan Dorsky <enigmoid@gmail.com>
2014-06-08 10:24:11 -0400

    Add test object

--- public/js/main.js
+++ public/js/main.js
@@ -3,5 +3,20 @@
 
 	var canvas = document.getElementById('poi-world')
 
-	console.log(canvas)
+	var ctx = canvas.getContext('2d')
+
+	window.world = new World(new vec2(0, -.1), 1, canvas, ctx)
+
+	var ball = new Ball(new vec2(50, 200), 1, 20, 'red')
+
+	world.addBody(ball)
+
+	window.clearCanvas = function(canvas, ctx) {
+		ctx.save();
+
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		
+		ctx.restore();
+	}
 })()
