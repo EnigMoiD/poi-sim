fc1fb910b5e281176e5b0c968ad5e1ddd072996d
Evan Dorsky <enigmoid@gmail.com>
2014-06-08 10:21:37 -0400

    Basic world

---
+++ public/js/world.js
@@ -0,0 +1,29 @@
+(function() {
+	window.World = function(g, dt, canvas, ctx) {
+		var w = this
+
+		w.g = g
+		w.dt = dt
+		w.canvas = canvas
+		w.ctx = ctx
+
+		w.bodies = []
+
+		w.addBody = function(body) {
+			body.addForce(w.g)
+			w.bodies.push(body)
+		}
+
+		w.step = function() {
+			clearCanvas(canvas, ctx)
+			for (var i in w.bodies) {
+				w.bodies[i].step(w.dt)
+				w.bodies[i].draw(w.ctx)
+			}
+		}
+
+		w.timer = setInterval(w.step, dt)
+
+		return w
+	}
+})()
